498de4611af4a3053150e8527a7e9d67e7b2b4d0
marco <marcogarofalo31@gmail.com>
2021-06-18 17:54:52 +0200

    add parameters in kcot and kiso

--- QC3_swave/detQC.py
+++ QC3_swave/detQC.py
@@ -31,28 +31,38 @@ from scipy import interpolate
 def kcot_2par(qk2,params):
     a0 = params[0]
     r0 = params[1]
-    return -1/a0 + r0*qk2
+    return -10/a0 + r0*qk2
     
     
-    
+@njit(fastmath=True)
+def kiso_2par(Delta,params):
+    a0 = params[0]
+    r0 = params[1]
+    return 800/a0 + r0*Delta
+        
 
-def QC3(e,L,nnP,kcot):
+def QC3(e, L, nnP, kcot_in, params_kcot, kiso, params_kiso ):
+
+    kcot=lambda qk2 : kcot_in(qk2,params_kcot)
+
     F00 = F3mat.F3mat00(e,L,0.5,nnP,kcot)
-    Kiso = 800
-    #Kiso= const + c1* qk^2
-#    K3 = 0*F00 + Kiso #useless
+    
+    ecm = np.sqrt(e**2 - (sums.norm(nnP)*2*math.pi/L)**2 )
+    Delta = (ecm**2 -9.0)/9.0
+    Kiso = kiso(Delta, params_kiso)
+    
     ones = np.ones(len(F00))
     Fiso = 1/(ones@F00@ones)
     return Fiso  + Kiso
 
 
-def find_sol(Estart, Eend, steps,L,nnP,kcot_in,params_kcot):
-    kcot=lambda qk2 : kcot_in(qk2,params_kcot)
+def find_sol(Estart, Eend, steps,L,nnP,kcot,params_kcot,kiso,params_kiso):
     energies = np.linspace(Estart, Eend,steps)
     param = []
     for i in range(steps):
-        param.append((energies[i],L,nnP,kcot))
-    res = list(itertools.starkcotmap(QC3, param))
+        param.append((energies[i],L,nnP,kcot,params_kcot ,kiso,params_kiso))
+     
+    res = list(itertools.starmap(QC3, param))
     print('Energies', energies)
     print('QC3iso', res)
     func = interpolate.InterpolatedUnivariateSpline(energies, res)
@@ -67,14 +77,12 @@ def find_sol(Estart, Eend, steps,L,nnP,kcot_in,params_kcot):
 #steps = 40
 
 #start = time.time()
-#sol= find_sol(Estart, Eend, steps,L,nnP,kcot_simple)  
+#sol= find_sol(Estart, Eend, steps,L,nnP,kcot_2par, (1,0),kiso_2par, (1,0))  
 #print('solution is:', 'E=',sol ,'Ecm=', np.sqrt(sol**2 - (sums.norm(nnP)*2*math.pi/L)**2))
 #end = time.time()
 #print('time is:', end - start, ' s')
 
-#ecm = np.sqrt(e**2 - (sums.norm(nnP)*2*math.pi/L)**2 )
-#delta = (ecm**2 -9.0)/9.0
-
+#
 
 def get_np_array(n1,n2,n3):
     return np.array([n1,n2,n3])
